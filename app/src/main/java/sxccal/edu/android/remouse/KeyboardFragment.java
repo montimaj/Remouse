90b43cb0366099c277d892467bf77fde4a67ea8f
montimaj <monti.majumdar@gmail.com>
2016-07-25 22:20:41 +0530

    Added Fragments. Improved UI

---
+++ app/src/main/java/sxccal/edu/android/remouse/KeyboardFragment.java
@@ -0,0 +1,16 @@
+package sxccal.edu.android.remouse;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class KeyboardFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_keyboard, container, false);
+    }
+}
--- app/src/main/java/sxccal/edu/android/remouse/MainActivity.java
+++ app/src/main/java/sxccal/edu/android/remouse/MainActivity.java
@@ -1,10 +1,9 @@
 package sxccal.edu.android.remouse;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -22,16 +21,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -79,17 +68,36 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
         if (id == R.id.nav_mouse) {
             // Handle the mouse action
+            fragment=new MouseFragment();
+            title="Remote Mouse";
+
         } else if (id == R.id.nav_keyboard) {
             // Handle the keyboard action
+            fragment=new KeyboardFragment();
+            title="Remote Keyboard";
 
         } else if (id == R.id.nav_connect) {
             // connection module
+            fragment=new ConnectionFragment();
+            title="Connect to PC";
 
         } else if (id == R.id.nav_about) {
             // App info
+            fragment=new AboutFragment();
+            title="About app";
+        }
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
