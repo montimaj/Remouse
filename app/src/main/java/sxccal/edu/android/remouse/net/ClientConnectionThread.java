14eab1b0ab014eabd88f25f3389679e199ea7a00
montimaj <monti.majumdar@gmail.com>
2016-09-04 18:29:34 +0530

    Fixed bugs in ClientConnectionThread

--- app/src/main/java/sxccal/edu/android/remouse/net/ClientConnectionThread.java
+++ app/src/main/java/sxccal/edu/android/remouse/net/ClientConnectionThread.java
@@ -7,11 +7,13 @@ import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.util.HashSet;
 
 import sxccal.edu.android.remouse.ConnectionFragment;
@@ -24,10 +26,11 @@ public class ClientConnectionThread implements Runnable {
 
     private Context mContext;
     private Activity mActivity;
-    private static ProgressDialog sProgressDialog;
-    public static DatagramSocket sDatagramSocket;
-
+    private DatagramSocket mDatagramSocket;
     private HashSet<String> mLocalDevices = new HashSet<>();
+
+    private static final int SOCKET_TIMEOUT = 5000;
+    private static ProgressDialog sProgressDialog;
 
     public ClientConnectionThread(Context context, Activity activity) {
         mContext = context;
@@ -59,29 +62,41 @@ public class ClientConnectionThread implements Runnable {
 
             do {
                 lock.acquire();
-                sDatagramSocket = new DatagramSocket(1235);
+                mDatagramSocket = new DatagramSocket(1235);
                 Log.d("ClientConnection: ", "Connecting...");
-                sDatagramSocket.setBroadcast(true);
+                mDatagramSocket.setBroadcast(true);
                 DatagramPacket datagramPacket = new DatagramPacket(new byte[32],32);
-                sDatagramSocket.receive(datagramPacket);
+                try {
+                    mDatagramSocket.setSoTimeout(SOCKET_TIMEOUT);
+                    mDatagramSocket.receive(datagramPacket);
+                } catch(SocketTimeoutException e) {
+                    lock.release();
+                    mDatagramSocket.close();
+                    break;
+                }
+
                 lock.release();
 
                 Log.d("ClientConnection: ", new String(datagramPacket.getData()));
-                Log.d("ClientConnection: ", ""+sDatagramSocket.getInetAddress());
+                Log.d("ClientConnection: ", ""+mDatagramSocket.getInetAddress());
 
                 InetAddress inetAddress = datagramPacket.getAddress();
                 mLocalDevices.add(inetAddress.toString().substring(1));
 
                 Log.d("ClientConnection: ", inetAddress.toString());
                 currentTime = System.currentTimeMillis();
-                sDatagramSocket.close();
-             }while((currentTime - startTime)/1000 < 5);
+                mDatagramSocket.close();
+             } while((currentTime - startTime) < SOCKET_TIMEOUT);
             handler.sendEmptyMessage(0);
 
             mActivity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    ConnectionFragment.addItems(mLocalDevices);
+                    if(mLocalDevices.isEmpty()) {
+                        Toast.makeText(mActivity, "No local devices found!", Toast.LENGTH_LONG).show();
+                    } else {
+                        ConnectionFragment.addItems(mLocalDevices);
+                    }
                 }
             });
         }catch(IOException e) { e.printStackTrace(); }
