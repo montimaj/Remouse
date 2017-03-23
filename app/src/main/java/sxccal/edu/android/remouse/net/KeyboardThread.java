bf4e1b996765250a0bae9024364a823cfd748da1
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2017-03-23 20:06:56 +0530

    Implement buffer using LinkedBlockingQueue

--- app/src/main/java/sxccal/edu/android/remouse/net/KeyboardThread.java
+++ app/src/main/java/sxccal/edu/android/remouse/net/KeyboardThread.java
@@ -1,7 +1,7 @@
 package sxccal.edu.android.remouse.net;
 
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
+
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static sxccal.edu.android.remouse.ConnectionFragment.sSecuredClient;
 
@@ -12,41 +12,29 @@ import static sxccal.edu.android.remouse.ConnectionFragment.sSecuredClient;
 public class KeyboardThread implements Runnable {
 
     private boolean mStopFlag;
-    private boolean mWait;
-    private LinkedList<String> mBuffer;
+    private LinkedBlockingQueue<String> mBuffer;
 
     public KeyboardThread() {
         mStopFlag = false;
-        mWait = false;
-        mBuffer = new LinkedList<>();
+        mBuffer = new LinkedBlockingQueue<>();
     }
 
     public void addToBuffer(String data) {
-        while(mWait);
-        setWait();
-        mBuffer.addLast(data);
-        clearWait();
+        try {
+            mBuffer.put(data);
+        } catch (InterruptedException e) { e.printStackTrace(); }
     }
 
     public void setStopFlag() { mStopFlag = true; }
-    private void setWait() { mWait = true; }
-    private void clearWait() { mWait = false; }
 
     @Override
     public void run() {
         while(!mStopFlag) {
-            if(!mWait) {
-                setWait();
-                if (!mBuffer.isEmpty()) {
-//                    Log.d("Keyboard Status", mBuffer.size() + "");
-                    try {
-                        String data = mBuffer.removeFirst();
-                        sSecuredClient.sendData("Key", data);
-//                    Log.d("Keyboard sent", data + "__>" + data.length());
-                    } catch (NoSuchElementException e) { e.printStackTrace(); }
-                }
-                clearWait();
-            }
+            try {
+                String data = mBuffer.take();
+                sSecuredClient.sendData("Key", data);
+//                Log.d("Keyboard sent", data + "__>" + data.length());
+            } catch (InterruptedException e) { e.printStackTrace(); }
         }
     }
 }
